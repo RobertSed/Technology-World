b8e4bb04ca6c37dcb2b18246cac8751a7229ca70
RobertSed <49155188+RobertSed@users.noreply.github.com>
2023-03-07 20:12:27 +0000

    Delete src directory

--- src/main.rs
+++
@@ -1,8 +0,0 @@
-pub mod database;
-
-fn main() {
-    let pool = database::get_connection_pool();
-    //let users = database::get_user(pool, "xx1x@xxx.com");
-    let users= database::get_all_users(pool);
-    println!("{:?}", users);
-}
