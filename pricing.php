93448bd4d86fb262a13489533cc7a1e8592da053
RobertSed <49155188+RobertSed@users.noreply.github.com>
2023-03-10 13:34:50 +0000

    New Webite design Added: courses page login register videos

---
+++ pricing.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <title>Technology World - VLE</title>
+    <meta property="og:image" content="assets/img/og.svg">
+    <meta name="description" content="Technology World provides beginner to advanced educational videos.">
+    <meta property="og:type" content="website">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans:400,500,600,700,800&amp;display=swap">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:400,500,700,900&amp;display=swap">
+    <link rel="stylesheet" href="assets/css/styles.min.css">
+</head>
+
+<body style="background: var(--bs-body-bg);--bs-body-bg: #ffffff;">
+    <nav class="navbar navbar-light navbar-expand-md sticky-top py-3" style="font-family: 'Open Sans', sans-serif;">
+        <div class="container"><a class="navbar-brand d-flex align-items-center" href="#" style="font-family: Roboto, sans-serif;"><span class="fw-bold" id="logo" style="font-family: Roboto, sans-serif;">Technology World</span></a><button data-bs-toggle="collapse" class="navbar-toggler" data-bs-target="#navcol-1"><span class="visually-hidden">Toggle navigation</span><span class="navbar-toggler-icon"></span></button>
+            <div class="collapse navbar-collapse" id="navcol-1">
+                <ul class="navbar-nav me-auto">
+                    <li class="nav-item"><a class="nav-link active" href="index.php">Home</a></li>
+                    <li class="nav-item"><a class="nav-link active" href="pricing.php">Pricing</a></li>
+                    <li class="nav-item"></li>
+                    <li class="nav-item"></li>
+                </ul><a class="btn btn-primary me-2" href="login.php">Login</a><a class="btn btn-primary me-2" href="register.php" style="background: var(--bs-navbar-brand-color);">Register</a>
+            </div>
+        </div>
+    </nav>
+    <section>
+        <div>
+            <div class="container py-4 py-xl-5">
+                <div class="row mb-5">
+                    <div class="col-md-8 col-xl-6 text-center mx-auto">
+                        <h2>Courses at affordable prices</h2>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="assets/js/script.min.js"></script>
+</body>
+
+</html>
